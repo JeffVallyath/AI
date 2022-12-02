3a891f8a955c5a564d64f4f1ecb361ef314aebf2
JeffVallyath <115042975+JeffVallyath@users.noreply.github.com>
2022-12-02 11:55:42 -0800

    Add files via upload

---
+++ AI project.py
@@ -0,0 +1,11 @@
+THE_QUESTION = input("How was your day?")
+
+if THE_QUESTION == "good":
+    print ("Glad to hear this")
+
+else:
+    MACHINE_LEARNING = input("I am a developing AI, so I couldn't figure out the answer to your question. Could you tell me a response that would fit your question?")
+
+if THE_QUESTION:
+    print (MACHINE_LEARNING)
+
